7e02e28814e126ed2fe522373a2157db90c21d5c
Manjunath Reddy <reddyvarimanjunathreddy2882004@gmail.com>
2025-09-15 15:26:59 +0530

    files

---
+++ my-counter-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App(){
+  const [count,setCount] = useState(0)
+    return(
+    <div className="App">
+      <h2>counter:{count}</h2>
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      
+      </div>)
+}
+export default App
---
+++ my-counter-app/src/temp.jsx
@@ -0,0 +1,14 @@
+function Welcome(prop) {
+    return <h2> welcome {prop.name}!</h2>;
+}
+function App(){
+    return (
+        <div>
+            <Welcome name="Man"></Welcome>
+            <welcome 
+            -*lcome>
+        </div>
+    )
+
+    }
+}
